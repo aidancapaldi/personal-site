a29f0b7a066f06232106950bae23787b923d92ed
aidancapaldi <aidancapaldi@outlook.com>
2024-04-27 22:38:31 -0400

    First linter err after successful build

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -41,7 +41,7 @@ describe('renders the app', () => {
   });
 
   it('should render the title', async () => {
-    expect(document.title).toBe("Aidan Capaldi");
+    expect(document.title).toBe('Aidan Capaldi');
   });
 
   it('can navigate to /about', async () => {
